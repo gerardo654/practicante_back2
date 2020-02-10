2d08f7b3b3ea7c0aa741731c7043a2589a779476
perez182 <www.davis.mx@hotmail.com>
2020-02-10 12:51:00 -0600

    borre todos los archivos

--- restapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Categoria_list, Categoria_detalle
-
-urlpatterns = [
-    path('Categorias/',Categoria_list, name ='Categoria_list'),
-    path('Categorias/<int:pk>',Categoria_detalle, name = 'Categoria_detalle')
-]
